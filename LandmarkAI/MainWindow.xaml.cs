4a9668d385445cd60343038a0028af686d307099
Eduardo Rosas <lalorosas@outlook.com>
2020-09-25 21:33:43 -0500

    Sending a request to the REST Service

--- LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -36,6 +39,27 @@ namespace LandmarkAI
             {
                 string fileName = dialog.FileName;
                 selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/4c4a70d5-7a8a-47dd-90ec-56aaca744a3e/classify/iterations/Iteration1/image";
+            string predictionKey = "8e2045f70e4246b7998eb5ce8827bd68";
+            string contentType = "application/octet-stream";
+            var imageFile = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+                
+                using(var content = new ByteArrayContent(imageFile))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+                    var response = await client.PostAsync(url, content);
+                }
             }
         }
     }
